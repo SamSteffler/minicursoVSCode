27654b2484d5a8780e2e47663e590a73e6ad9f63
Samuel Steffler <samuel.steffler@acad.ufsm.br>
2025-04-03 19:59:13 -0300

    add: launch.json modify: main.c, tasks.json

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+  char *ptr = argv[1];
+  int val = atoi(argv[2]); // Convert argv[2] to an integer
+  printf("Hello, World! Hi, %s, now it's %d o'clock.\n", ptr, val);
   return 0;
 }
